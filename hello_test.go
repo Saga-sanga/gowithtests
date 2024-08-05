4abbde8d5ff6a29561812af203ad058a22f3eabd
Saga-sanga <vulcan248@gmail.com>
2024-08-05 20:34:13 +0530

    refactor hello code

--- hello_test.go
+++ hello_test.go
@@ -4,13 +4,28 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("Sanga")
-		want := "Hello Sanga"
+		got := Hello("Sanga", "")
+		want := "Hello Sanga!"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello World!' when an empty string is passed", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello World!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Karlos", "Spanish")
+		want := "Hola Karlos!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Karles", "French")
+		want := "Bonjour Karles!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Mizo", func(t *testing.T) {
+		got := Hello("Karles", "Mizo")
+		want := "Chibai Karles!"
 		assertCorrectMessage(t, got, want)
 	})
 }
