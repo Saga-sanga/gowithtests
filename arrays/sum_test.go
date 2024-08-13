323335bb12eb599dfcf65da18415e525c0be3852
Saga-sanga <vulcan248@gmail.com>
2024-08-13 23:00:35 +0530

    create test for adding array and testable method

---
+++ arrays/sum_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 3, 4, 6, 2}
+
+		got := Sum(numbers)
+		want := 16
+
+		assertErrorMessage(t, got, want, numbers)
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		assertErrorMessage(t, got, want, numbers)
+	})
+}
+
+func assertErrorMessage(t testing.TB, got, want int, numbers []int) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
