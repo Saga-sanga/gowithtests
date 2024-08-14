7bc7bfb2fbfda62bb0ead2e4d1be998fae15e7c4
Saga-sanga <vulcan248@gmail.com>
2024-08-14 12:40:42 +0530

    refactor test and add code to Sum all trails

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -33,3 +33,23 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 5})
+		want := []int{5, 14}
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 3, 5})
+		want := []int{0, 8}
+		checkSums(t, got, want)
+	})
+}
