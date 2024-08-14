2e29e507d848dd8e12a79bc194b47bd7bd0e863d
Saga-sanga <vulcan248@gmail.com>
2024-08-14 12:02:48 +0530

    add test to sum

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -19,5 +22,14 @@ func assertErrorMessage(t testing.TB, got, want int, numbers []int) {
 
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
