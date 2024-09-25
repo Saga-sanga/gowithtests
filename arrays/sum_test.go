fa7a1c63b94ef9a8e5589bc6652a1e3919b7d0be
Saga-sanga <vulcan248@gmail.com>
2024-09-25 16:09:42 +0530

    chore: update test to use new slice.Equal

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"reflect"
+	"slices"
 	"testing"
 )
 
@@ -29,7 +30,8 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if !reflect.DeepEqual(got, want) {
+	t.Logf("Using slices.Equal")
+	if !slices.Equal(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
