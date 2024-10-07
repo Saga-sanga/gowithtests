f87d9fbc78183ee20ea012bfd7e85f5a3cf1ce1a
Saga-sanga <vulcan248@gmail.com>
2024-10-07 22:22:42 +0530

    refactor: delete default sleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -14,12 +14,6 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
-
-func (d DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 type ConfigurableSleeper struct {
 	duration time.Duration
 	sleep    func(time.Duration)
@@ -38,6 +32,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
