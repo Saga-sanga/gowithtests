d045c81df2be109b5020cb975cf1f351a25c1e4c
Saga-sanga <vulcan248@gmail.com>
2024-10-29 21:59:07 +0530

    feat: add demo code for zero division

---
+++ zero/zero.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(secondsinraidans())
+}
+
+func zero() float64 {
+	return 0.0
+}
+
+func secondsinraidans() float64 {
+	return (math.Pi / (30 / (float64(zero()))))
+}
